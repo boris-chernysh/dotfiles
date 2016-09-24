6747766e4bd8be224ad8044a8e6f045b3f52fe25
Boris Chernysh <borischernysh31@gmail.com>
2016-09-24 12:02:27 +0300

    some order to folders struct

--- helpers/create_links.sh
+++ helpers/create_links.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ln -s "$PWD/bobbynotrussell.zsh-theme" "$HOME/.oh-my-zsh/themes"
-ln -s "$PWD/zshrc.oh-my-zsh" "$HOME/.zshrc"
-ln -s "$PWD/vimrc" "$HOME/.vimrc"
+ln -s "$PWD/zsh/bobbynotrussell.zsh-theme" "$HOME/.oh-my-zsh/themes"
+ln -s "$PWD/zsh/zshrc.oh-my-zsh" "$HOME/.zshrc"
+ln -s "$PWD/vim/vimrc" "$HOME/.vimrc"
 ln -s "$PWD/tmux.conf" "$HOME/.tmux.conf"
