8421b958c55623aecc8e1d0263408f681c311dbc
b.chernysh <b.chernysh@tinkoff.ru>
2020-01-21 17:54:21 +0300

    added script for multi-monitors

---
+++ i3/scripts/screenlayout/3.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+xrandr --output eDP1 --primary --mode 1920x1080 --pos 1400x1406 --rotate normal --output DP1 --mode 2560x1080 --pos 0x0 --rotate left --output HDMI1 --mode 2560x1080 --pos 1080x326 --rotate normal --output HDMI2 --off --output VIRTUAL1 --off
