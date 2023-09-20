8e60a5a5efbc876678dcc64cc3181834d7d60c7f
Boris Chernysh <borischernysh31@gmail.com>
2023-09-20 17:33:15 +0300

    add mobile i3 view

--- polybar/run.sh
+++ polybar/run.sh
@@ -1,8 +1,38 @@
 #!/usr/bin/env bash
 
-killall -q polybar
+mkdir -p ~/.polybar/pid/
 
-echo "---" | tee -a /tmp/polybar.log
-polybar -c ~/dotfiles/polybar/config topbar >>/tmp/polybar.log 2>&1 &
+if [[ "$DISPLAY_MODE" == "mobile" ]]; then
+    ########################
+    # Bars for mobile view #
+    ########################
+    polybar-msg -p $(cat ~/.polybar/pid/mobile_top) cmd quit
 
-echo "Bars launched..."
+    echo "---" | tee -a /tmp/polybar_mobile_top.log
+
+    polybar -c ~/dotfiles/polybar/config.ini mobile_top >> /tmp/polybar_mobile_top.log 2>&1 &
+    echo $! > ~/.polybar/pid/mobile_top
+
+    echo "Mobile topbar launched..."
+
+    polybar-msg -p $(cat ~/.polybar/pid/mobile_bottom) cmd quit
+
+    echo "---" | tee -a /tmp/polybar_mobile_bottom.log
+
+    polybar -c ~/dotfiles/polybar/config.ini mobile_bottom >> /tmp/polybar_mobile_bottom.log 2>&1 &
+    echo $! > ~/.polybar/pid/mobile_bottom
+
+    echo "Mobile bottombar launched..."
+else
+    ######################
+    # Topbar for desktop #
+    ######################
+    polybar-msg -p $(cat ~/.polybar/pid/topbar) cmd quit
+
+    echo "---" | tee -a /tmp/polybar_topbar.log
+
+    polybar -c ~/dotfiles/polybar/config.ini topbar >> /tmp/polybar_topbar.log 2>&1 &
+    echo $! > ~/.polybar/pid/topbar
+
+    echo "Topbar launched..."
+fi
